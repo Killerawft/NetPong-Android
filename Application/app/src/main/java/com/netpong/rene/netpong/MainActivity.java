e2725473409a8435ea11677c44d8f846ff970d03
Killerawft <Renelue@gmx.de>
2017-01-29 22:25:19 +0100

    UDP Client Test

--- Application/app/src/main/java/com/netpong/rene/netpong/MainActivity.java
+++ Application/app/src/main/java/com/netpong/rene/netpong/MainActivity.java
@@ -12,20 +12,37 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.Formatter;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,31 +51,40 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         final Button bCon1 = (Button) findViewById(R.id.bCon1);
         final Button bCon2 = (Button) findViewById(R.id.bCon2);
-
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final EditText etName = (EditText) findViewById(R.id.etName);
 
         bCon1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-            }
+
+                try {
+                    if (connectPlayer(etName.getText().toString(), 1))
+                        bCon2.setEnabled(false);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+        }
         });
 
         bCon2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                try {
+                    if (connectPlayer(etName.getText().toString(), 2))
+                        bCon1.setEnabled(false);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -83,33 +109,89 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void client() throws IOException{
+    public void ConPlay1(View view) {
 
+    }
 
-        DatagramSocket client_socket = new DatagramSocket(2362);
-        InetAddress IPAddress =  InetAddress.getByName("10.80.1.95");
+    public void ConPlay2(View view) {
 
-        //while (true)
-        // {
-        send_data = str.getBytes();
-        //System.out.println("Type Something (q or Q to quit): ");
+    }
 
-        DatagramPacket send_packet = new DatagramPacket(send_data,str.length(), IPAddress, 2362);
+    public boolean connectPlayer(String Name, int PlayerNr) throws IOException {
+
+        int GamePort = 2222;
+
+        byte[] send_bytes;
+        String send_data;
+        byte[] receiveData = null;
+        String recieveString;
+        final EditText etIP = (EditText) findViewById(R.id.etIP);
+
+        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+        int OwnIp = wifiInfo.getIpAddress();
+
+        DatagramSocket client_socket = new DatagramSocket(GamePort);
+        InetAddress IPAddress = InetAddress.getByName(etIP.getText().toString());
+
+        send_data = "N" + Integer.toString(PlayerNr) + ";" + Name + ";" + Integer.toString(OwnIp);
+
+        send_bytes = send_data.getBytes();
+
+        DatagramPacket send_packet = new DatagramPacket(send_bytes, send_data.length(), IPAddress, GamePort);
         client_socket.send(send_packet);
-        //chandra
+
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         client_socket.receive(receivePacket);
-        modifiedSentence = new String(receivePacket.getData());
-        //System.out.println("FROM SERVER:" + modifiedSentence);
-        if(modifiedSentence.charAt(2)=='%')
-            txt5.setText(modifiedSentence.substring(0, 3));
-        else
-            txt1.setText(modifiedSentence);
-        modifiedSentence=null;
+
         client_socket.close();
 
-        // }
+        recieveString = new String(receivePacket.getData());
+        //System.out.println("FROM SERVER:" + modifiedSentence);
+        if (recieveString == "NP_OK") {
+            recieveString = null;
+            return true;
+        }
+        else {
+            recieveString = null;
+            return false;
+        }
+    }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
 
